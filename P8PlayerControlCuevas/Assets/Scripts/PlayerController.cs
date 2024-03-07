1df0b24053ff4178b728088af5df7fa5d95f3162
761678 <pedrocuevasarana123@gmail.com>
2024-03-07 15:31:42 -0800

    Done
    with game controler

--- P8PlayerControlCuevas/Assets/Scripts/PlayerController.cs
+++ P8PlayerControlCuevas/Assets/Scripts/PlayerController.cs
@@ -4,16 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //Private Variables
+    private float speed = 10f;
+   private float turnSpeed = 25.0f;
+   private float horizontalInput;
+   private float forwardInput;
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
     // Update is called once per frame
     void Update()
     {
+        //This is wwhere we get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
         //Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //We turn the vehicle
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+        if(Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled =!mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
     }
 }
